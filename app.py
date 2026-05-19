c7d65b843de35340a396a807a9fd3509800da448
Chava Epstein <22501243@stu.mmu.ac.uk>
2026-05-19 14:09:52 +0100

    SQL initialisation

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+
 
 app = Flask(__name__)
+app .config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
 
 @app.get("/")
 def home():
