3a998a4cc53bbee730441e1f58979ac30479495a
Chava Epstein <22501243@stu.mmu.ac.uk>
2026-05-20 12:27:23 +0100

    configured sql database

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app .config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(120), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, default=date.today)
+
 with app.app_context():
     db.create_all()
 
